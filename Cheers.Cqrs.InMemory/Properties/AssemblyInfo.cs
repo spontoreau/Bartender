c7dc6dce0b97da18a3a9c451f847e55a2d450bf7
pontoreau <pontoreau.sylvain@gmail.com>
2016-06-12 10:48:41 +0200

    Update AssemblyInfo

--- Cheers.Cqrs.InMemory/Properties/AssemblyInfo.cs
+++ Cheers.Cqrs.InMemory/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Cheers.Cqrs.InMemory")]
 [assembly: AssemblyDescription("Cheers in memory CQRS implementation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CheersTeam")]
+[assembly: AssemblyCompany("Cheers")]
 [assembly: AssemblyProduct("Cheers")]
-[assembly: AssemblyCopyright("© 2016 CheersTeam")]
+[assembly: AssemblyCopyright("© 2016 Cheers")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
