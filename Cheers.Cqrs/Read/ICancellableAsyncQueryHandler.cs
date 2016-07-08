ffd652778939a2f228f61440acaad9818b44e8da
pontoreau <pontoreau.sylvain@gmail.com>
2016-07-08 09:47:22 +0200

    Add cancellable handlers & dispatchers

---
+++ Cheers.Cqrs/Read/ICancellableAsyncQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cheers.Cqrs.Read
+{
+    /// <summary>
+    /// Define a cancellable asynchronous query handler.
+    /// </summary>
+    /// <typeparam name="TQuery">Query type</typeparam>
+    /// <typeparam name="TReadModel">ReadModel type</typeparam>
+    public interface ICancellableAsyncQueryHandler<TQuery, TReadModel>
+        where TQuery : IQuery
+        where TReadModel : IReadModel
+    {
+        /// <summary>
+        /// Handle a query asynchronously
+        /// </summary>
+        /// <param name="query">Query to handle</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Enumerable of ReadModel</returns>
+        Task<IEnumerable<TReadModel>> Handle(TQuery query, CancellationToken cancellationToken);
+    }
+}
+
