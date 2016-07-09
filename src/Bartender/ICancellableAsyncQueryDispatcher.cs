256517b2a3f67cd9add49643f91520d47ee82052
pontoreau <pontoreau.sylvain@gmail.com>
2016-07-09 14:57:03 +0200

    Add cancellable async query handling

---
+++ src/Bartender/ICancellableAsyncQueryDispatcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bartender
+{
+    /// <summary>
+    /// Define a cancellable asynchronous query dispatcher.
+    /// </summary>
+    public interface ICancellableAsyncQueryDispatcher
+    {
+        /// <summary>
+        /// Dispatch a query asynchronously.
+        /// </summary>
+        /// <param name="query">Query to dispatch</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        Task<TReadModel> Dispatch<TQuery, TReadModel>(TQuery query, CancellationToken cancellationToken)
+            where TQuery : IQuery;
+    }
+}
+
