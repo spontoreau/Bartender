6685553f08dda9bf97180ffe05f3e083a1fdb877
pontoreau <pontoreau.sylvain@gmail.com>
2016-07-09 15:16:10 +0200

    Add cancellable async command handling

---
+++ src/Bartender/ICancellableAsyncCommandDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bartender
+{
+    /// <summary>
+    /// Define a cancellable asynchronous command dispatcher.
+    /// </summary>
+    public interface ICancellableAsyncCommandDispatcher
+    {
+        /// <summary>
+        /// Dispatch a command asynchronously.
+        /// </summary>
+        /// <param name="command">Command to dispatch</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Result</returns>
+        Task<TResult> Dispatch<TCommand, TResult>(TCommand command, CancellationToken cancellationToken)
+            where TCommand : ICommand;
+
+        /// <summary>
+        /// Dispatch a command asynchronously.
+        /// </summary>
+        /// <param name="command">Command to dispatch</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        Task Dispatch<TCommand>(TCommand command, CancellationToken cancellationToken)
+            where TCommand : ICommand;
+    }
+}
+
