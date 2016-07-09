19af3e0351fc7372ada8200d0643a797ced028f1
pontoreau <pontoreau.sylvain@gmail.com>
2016-07-09 14:51:08 +0200

    Add async query handling

---
+++ src/Bartender/IAsyncQueryDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bartender
+{
+    /// <summary>
+    /// Define an asynchronous query dispatcher
+    /// </summary>
+    public interface IAsyncQueryDispatcher
+    {
+        /// <summary>
+        /// Dispatch a query asynchronously.
+        /// </summary>
+        /// <param name="query">Query to dispatch</param>
+        Task<TReadModel> DispatchAsync<TQuery, TReadModel>(TQuery query)
+            where TQuery : IQuery;
+    }
+}
+
---
+++ src/Bartender/IAsyncQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Bartender
+{
+    /// <summary>
+    /// Define an asynchronous query handler
+    /// </summary>
+    public interface IAsyncQueryHandler<TQuery, TReadModel> 
+        where TQuery : IQuery
+    {
+        /// <summary>
+        /// Handle a query asynchronously
+        /// </summary>
+        /// <param name="query">Query to handle</param>
+        /// <returns>ReadModel</returns>
+        Task<TReadModel> HandleAsync(TQuery query);
+    }
+}
+
