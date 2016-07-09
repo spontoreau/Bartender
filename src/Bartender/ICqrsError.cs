d2b74cd992e8a2df72da5f45be386e446c4e5b6b
pontoreau <pontoreau.sylvain@gmail.com>
2016-07-09 14:21:28 +0200

    Add CQRS error interface

---
+++ src/Bartender/ICqrsError.cs
@@ -0,0 +1,21 @@
+﻿namespace Bartender
+{
+    /// <summary>
+    /// Define a CQRS error
+    /// </summary>
+    public interface ICqrsError
+    {
+        /// <summary>
+        /// Gets the code.
+        /// </summary>
+        /// <value>The code.</value>
+        int Code { get; }
+
+        /// <summary>
+        /// Gets the error.
+        /// </summary>
+        /// <value>The error.</value>
+        string Error { get; }
+    }
+}
+
