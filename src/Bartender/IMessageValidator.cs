7c08613731c0a44ca7740115ca3924fc6fe0fb2e
pontoreau <pontoreau.sylvain@gmail.com>
2016-07-09 14:20:46 +0200

    Add message validator interface

---
+++ src/Bartender/IMessageValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace Bartender
+{
+    /// <summary>
+    /// Define a message validator
+    /// </summary>
+    public interface IMessageValidator<TMessage> where TMessage : IMessage
+    {
+        /// <summary>
+        /// Validate the specified message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        void Validate(TMessage message);
+
+        /// <summary>
+        /// True if the message is valid, otherwise false
+        /// </summary>
+        /// <param name="message">Message.</param> 
+        /// <returns>True if the message is valid, otherwise false</returns>
+        bool IsValid(TMessage message);
+    }
+}
+
